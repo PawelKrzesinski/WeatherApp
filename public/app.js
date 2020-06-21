15a1363d287f8dece95f6ab5a648bc119fc4528e
Pawel <52802778+PawelKrzesinski@users.noreply.github.com>
2020-06-21 17:03:35 +0100

    Update app.js
    Hid API key.
    Added helper function for unix timestamp converting.
    Changed displayed information so it's more readible and easier to understand.

--- public/app.js
+++ public/app.js
@@ -1,4 +1,4 @@
-
+const API_KEY = "4d2244a3474ed68ea9dcb302b81d1e59";
 const temp__div = document.getElementById('temp');
 const sunrise__div = document.getElementById('sunrise');
 const sunset__div = document.getElementById('sunset');
@@ -9,22 +9,33 @@ const humidity__div = document.getElementById('humidity');
 const feelsLike__div = document.getElementById('feels__like');
 const windSpeed__div = document.getElementById('wind__speed');
 const timezone__div = document.getElementById('timezone');
+const submit = document.getElementById('submit');
+let long;
+let lat;
 
 document.addEventListener('DOMContentLoaded', () => {
-	let long;
-	let lat;
-
 	if(navigator.geolocation){
-		navigator.geolocation.getCurrentPosition(position => {
+		navigator.geolocation.getCurrentPosition(async position => {
 			long = position.coords.longitude;
 			lat = position.coords.latitude;
 			let url = `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${long}&exclude={minutely,hourly,daily}&appid=${API_KEY}&units=metric`
-			fetch(url)
-			.then(response => {
+			const items = { lat, long };
+			const options = {
+				method: 'POST',
+				headers: {
+					'Content-Type': 'application/json'
+				},
+				body: JSON.stringify(items)
+			};
+
+			const response = await fetch('/api', options);
+			const json = await response.json();
+			console.log(json);
+			
+			fetch(url).then(response => {
 				let data = response.json();
 				return data;
-			})
-			.then(data =>{
+			}).then(data =>{
 				let sunrise = data.current.sunrise;
 				let sunset = data.current.sunset;
 				let desc = data.current.weather[0].main;
@@ -38,21 +49,30 @@ document.addEventListener('DOMContentLoaded', () => {
 				let information = [sunrise, sunset, desc, weatherIcon, pressure, humidity, feelsLike, windSpeed, timezone, temp]
 				return information;
 			}).then((information) => {
-				temp__div.innerHTML = information[9];
-				sunrise__div.innerHTML = information[0];
-				sunset__div.innerHTML = information[1];
+				sunrise__div.innerHTML = `Sunrise: `+ unixToDate(information[0]);
+				sunset__div.innerHTML = `Sunset: ` + unixToDate(information[1]);
 				desc__div.innerHTML = information[2];
-				weatherIcon__div.innerHTML = information[3];
-				pressure__div.innerHTML = information[4];
-				humidity__div.innerHTML = information[5];
-				feelsLike__div.innerHTML = information[6];
-				windSpeed__div.innerHTML = information[7];
+				weatherIcon__div.innerHTML = `<img src='http://openweathermap.org/img/wn/${information[3]}@2x.png' class='icon__image'/>`;
+				pressure__div.innerHTML = `Pressure: ` + information[4] + ` Pa`;
+				humidity__div.innerHTML = `Humidity: ` + information[5] + ` %`;
+				feelsLike__div.innerHTML = `Feels like: ` + information[6] + `&degC`;
+				windSpeed__div.innerHTML = `Wind speed: ` + information[7] + ` m/s`;
 				timezone__div.innerHTML = information[8];
+				temp__div.innerHTML = `Temp: ` + information[9] + `&degC`;
 			})
 			.catch(err => {
 				console.log(err);
 			});
 		})
-		
 	}
 })
+
+function unixToDate(unix) {
+	const date = new Date(unix * 1000);
+	const hours = date.getHours().toString().padStart(2, '0');
+	const minutes = date.getMinutes().toString().padStart(2, '0');
+	const seconds = date.getSeconds().toString().padStart(2, '0');
+	return hours + ":" + minutes + ":" + seconds;
+}
+
+
