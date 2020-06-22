e7149c365c6b97a171356bbaaa52416e9534e924
Pawel <52802778+PawelKrzesinski@users.noreply.github.com>
2020-06-22 21:40:51 +0100

    Update server.js
    Added a request to API for the data
    Data from API converted into json format and sent to client-side app

--- server.js
+++ server.js
@@ -1,17 +1,34 @@
 const express = require('express');
 const app = express();
+const fetch = require('node-fetch')
 require('dotenv').config();
-
-app.listen(3000, () => console.log("Ha ha i'm behind you"))
+const API_KEY = process.env.API_KEY;
+app.listen(3000, () => console.log("Starting Server : http://localhost:3000"))
 app.use(express.static('public'));
 app.use(express.json({ limit: '1mb' }));
-
 app.post('/api', (request, response) => {
 	console.log(request.body);
 	const data = request.body;
 	response.json({
 		status: 'success',
 		latitude: data.lat,
-		longitude: data.long
+		longitude: data.lon
 	})
 })
+
+app.get('/weather/:latlon', async (request, response) => {
+	const latlon = request.params.latlon.split(',');
+	console.log(latlon);
+	const lat = latlon[0];
+	const lon = latlon[1];
+	console.log(lat, lon);
+	const url = `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&exclude={minutely,hourly,daily}&appid=${API_KEY}&units=metric`
+	const fetch_response = await fetch(url);
+	const json = await fetch_response.json();
+	response.json(json);
+	console.log(json);
+	
+})
+
+
+
