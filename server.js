d5953f8b6722f90b35c810ee42c602d02707fa7e
Pawel <52802778+PawelKrzesinski@users.noreply.github.com>
2020-06-25 11:03:28 +0100

    Update server.js

--- server.js
+++ server.js
@@ -20,10 +20,8 @@ app.post('/api', (request, response) => {
 
 app.get('/weather/:latlon', async (request, response) => {
 	const latlon = request.params.latlon.split(',');
-	console.log(latlon);
 	const lat = latlon[0];
 	const lon = latlon[1];
-	console.log(lat, lon);
 	const url = `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&exclude={minutely,hourly,daily}&appid=${API_KEY}&units=metric`
 	const fetch_response = await fetch(url);
 	const json = await fetch_response.json();
